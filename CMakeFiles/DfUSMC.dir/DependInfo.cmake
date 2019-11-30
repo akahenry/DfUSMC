e030ca991ce69d52457ce4458a8fe3f7e92c4f2d
MSWLandi <fakepsnmarcos@gmail.com>
2019-11-30 15:10:15 -0300

    cmake inicial

---
+++ CMakeFiles/DfUSMC.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/marcos/Proj/FPI_T3/repo/DfUSMC/Yang_cvpr12/ctmf.c" "/home/marcos/Proj/FPI_T3/repo/DfUSMC/CMakeFiles/DfUSMC.dir/Yang_cvpr12/ctmf.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "GFLAGS_IS_A_DLL=0"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "/usr/include/eigen3"
+  "Yang_cvpr12"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/marcos/Proj/FPI_T3/repo/DfUSMC/DfUSMC.cpp" "/home/marcos/Proj/FPI_T3/repo/DfUSMC/CMakeFiles/DfUSMC.dir/DfUSMC.cpp.o"
+  "/home/marcos/Proj/FPI_T3/repo/DfUSMC/Yang_cvpr12/qx_basic.cpp" "/home/marcos/Proj/FPI_T3/repo/DfUSMC/CMakeFiles/DfUSMC.dir/Yang_cvpr12/qx_basic.cpp.o"
+  "/home/marcos/Proj/FPI_T3/repo/DfUSMC/Yang_cvpr12/qx_mst_kruskals_image.cpp" "/home/marcos/Proj/FPI_T3/repo/DfUSMC/CMakeFiles/DfUSMC.dir/Yang_cvpr12/qx_mst_kruskals_image.cpp.o"
+  "/home/marcos/Proj/FPI_T3/repo/DfUSMC/Yang_cvpr12/qx_tree_filter.cpp" "/home/marcos/Proj/FPI_T3/repo/DfUSMC/CMakeFiles/DfUSMC.dir/Yang_cvpr12/qx_tree_filter.cpp.o"
+  "/home/marcos/Proj/FPI_T3/repo/DfUSMC/Yang_cvpr12/qx_tree_upsampling.cpp" "/home/marcos/Proj/FPI_T3/repo/DfUSMC/CMakeFiles/DfUSMC.dir/Yang_cvpr12/qx_tree_upsampling.cpp.o"
+  "/home/marcos/Proj/FPI_T3/repo/DfUSMC/main.cpp" "/home/marcos/Proj/FPI_T3/repo/DfUSMC/CMakeFiles/DfUSMC.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GFLAGS_IS_A_DLL=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "/usr/include/eigen3"
+  "Yang_cvpr12"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/DfUSMC.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DfUSMC.dir/main.cpp.o"
+  "CMakeFiles/DfUSMC.dir/DfUSMC.cpp.o"
+  "CMakeFiles/DfUSMC.dir/Yang_cvpr12/qx_tree_upsampling.cpp.o"
+  "CMakeFiles/DfUSMC.dir/Yang_cvpr12/qx_tree_filter.cpp.o"
+  "CMakeFiles/DfUSMC.dir/Yang_cvpr12/qx_mst_kruskals_image.cpp.o"
+  "CMakeFiles/DfUSMC.dir/Yang_cvpr12/qx_basic.cpp.o"
+  "CMakeFiles/DfUSMC.dir/Yang_cvpr12/ctmf.c.o"
+  "DfUSMC.pdb"
+  "DfUSMC"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/DfUSMC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
